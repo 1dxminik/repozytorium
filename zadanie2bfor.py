60b867e2653793798867f929f80f2671e3feb25e
Dominik <dominik.czapla2020@gmail.com>
2025-11-03 12:29:49 +0100

    zadanie 2b

---
+++ zadanie2bfor.py
@@ -0,0 +1,19 @@
+
+"""
+numbers = [1, 2, 3, 4, 5]
+
+def Liczby(numbers):
+    for i in numbers:
+        i = i * 2
+        print(i)
+
+Liczby(numbers)
+
+
+"""
+
+liczby = [1, 2, 3, 4, 5]
+
+lista = [n*2 for n in liczby]
+
+print(lista)
