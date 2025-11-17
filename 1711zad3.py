4631dff237cd79224cf6d1e0b8035e175c5c75b5
Dominik <dominik.czapla2020@gmail.com>
2025-11-17 14:39:11 +0100

    zrobione

---
+++ 1711zad3.py
@@ -0,0 +1,10 @@
+#tak naprawde to zadanie 5
+
+a = list(input("Podaj elementy listy: ").split())
+b = int(input("Napisz ile jest elementów listy: "))
+
+if len(a) != b:
+    print(False)
+
+else:
+    print(True)
